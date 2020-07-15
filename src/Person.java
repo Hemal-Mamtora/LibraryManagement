d9f68b72dcdb105903b787643cc37c0ffc474ef0
Hemal-Mamtora <hemalmamtora1@gmail.com>
2020-07-15 14:16:05 +0530

    Person

--- src/Person.java
+++ src/Person.java
@@ -1,12 +1,52 @@
 
 public class Person {
-	String id;
+	int id;
 	String name;
 	String username;
 	String pwd;
 	boolean isLibrarian;
 	
-	Person(String id, String name, String username, String pwd, boolean isLibrarian){
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public boolean isLibrarian() {
+		return isLibrarian;
+	}
+
+	public void setLibrarian(boolean isLibrarian) {
+		this.isLibrarian = isLibrarian;
+	}
+
+	Person(int id, String name, String username, String pwd, boolean isLibrarian){
 		this.id = id;
 		this.name = name;
 		this.username = username;
